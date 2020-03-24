ad611938f65329828baa367c8f6e23b636e6c6cc
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-25 00:07:52 +0200

    add about menu and about submenu options

--- notepad.py
+++ notepad.py
@@ -17,6 +17,18 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход")
 main_menu.add_cascade(label="Файл", menu=file_menu)
 
+# About
+help_menu = Menu(main_menu, tearoff=0)
+help_menu_sub = Menu(help_menu, tearoff=0)
+
+help_menu_sub.add_command(label="Онлайн")
+help_menu_sub.add_command(label="Оффлайн")
+
+help_menu.add_cascade(label="Помощь", menu=help_menu_sub)
+help_menu.add_command(label="О программе")
+
+main_menu.add_cascade(label="Справка", menu=help_menu)
+
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
 
