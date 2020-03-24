504935f68822f4f7eb190fde3ce476f427ff762c
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-25 00:13:20 +0200

    add new options -> text and scrollbar

--- notepad.py
+++ notepad.py
@@ -6,8 +6,8 @@ root.geometry('600x400+1000+300')
 main_menu = Menu(root)
 root.config(menu=main_menu)
 
-# main_menu.add_command(label="File")
-# main_menu.add_command(label="About")
+def about_program():
+  print('ABOUT')
 
 # File
 file_menu = Menu(main_menu, tearoff=0)
@@ -25,11 +25,18 @@ help_menu_sub.add_command(label="Онлайн")
 help_menu_sub.add_command(label="Оффлайн")
 
 help_menu.add_cascade(label="Помощь", menu=help_menu_sub)
-help_menu.add_command(label="О программе")
+help_menu.add_command(label="О программе", command=about_program)
 
 main_menu.add_cascade(label="Справка", menu=help_menu)
 
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
+
+t = Text(f_text, bg="#343D46", fg="#C6DEC1", padx=10, pady=10, wrap=WORD, insertbackground="#EDA756", selectbackground="#4E5A65", width=30, spacing3=10)
+t.pack(fill=BOTH, expand=1, side=LEFT)
+
+scroll = Scrollbar(f_text, command=t.yview)
+scroll.pack(fill=Y, side=LEFT)
+t.config(yscrollcommand=scroll.set)
 
 root.mainloop()
