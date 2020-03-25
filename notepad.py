3c550407f119eff735e48da203ab27aac33519d9
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-25 23:49:06 +0200

    realize theme change function

--- notepad.py
+++ notepad.py
@@ -9,6 +9,12 @@ root.config(menu=main_menu)
 def about_program():
   print('ABOUT')
 
+def change_theme(theme):
+  t['bg'] = theme_colors[theme]['text_bg']
+  t['fg'] = theme_colors[theme]['text_fg']
+  t['insertbackground'] = theme_colors[theme]['cursor']
+  t['selectbackground'] = theme_colors[theme]['select_bg']
+
 # File
 file_menu = Menu(main_menu, tearoff=0)
 file_menu.add_command(label="Открыть")
@@ -21,8 +27,8 @@ main_menu.add_cascade(label="Файл", menu=file_menu)
 theme_menu = Menu(main_menu, tearoff=0)
 theme_menu_sub = Menu(theme_menu, tearoff=0)
 
-theme_menu_sub.add_command(label="Light theme")
-theme_menu_sub.add_command(label="Dark theme")
+theme_menu_sub.add_command(label="Light theme", command=lambda: change_theme('light'))
+theme_menu_sub.add_command(label="Dark theme", command=lambda: change_theme('dark'))
 
 theme_menu.add_cascade(label="Оформление", menu=theme_menu_sub)
 theme_menu.add_command(label="О программе", command=about_program)
@@ -33,9 +39,28 @@ main_menu.add_cascade(label="Разное", menu=theme_menu)
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
 
+theme_colors = {
+  "dark": {
+    "text_bg": "#343D46", "text_fg": "#C6DEC1", "cursor": "#EDA756", "select_bg": "#4E5A65"
+  },
+  "light": {
+    "text_bg": "#fff", "text_fg": "#000", "cursor": "#8000ff", "select_bg": "#777"
+  }
+}
 
-
-t = Text(f_text, bg="#343D46", fg="#C6DEC1", padx=10, pady=10, wrap=WORD, insertbackground="#EDA756", selectbackground="#4E5A65", width=30, spacing3=10)
+t = Text(
+  f_text,
+  bg=theme_colors['dark']['text_bg'],
+  fg=theme_colors['dark']['text_fg'],
+  padx=10,
+  pady=10,
+  wrap=WORD,
+  insertbackground=theme_colors['dark']['cursor'],
+  selectbackground=theme_colors['dark']['select_bg'],
+  width=30,
+  spacing3=10,
+  font=("Courier New", 11)
+  )
 t.pack(fill=BOTH, expand=1, side=LEFT)
 
 # Scrollbar
